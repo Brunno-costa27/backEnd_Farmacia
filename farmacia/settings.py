4a6965c9039034ed48ced5e48bd938a02c4c17f4
Brunno Costa <sbrunno.costa@gmail.com>
2022-08-06 16:55:43 -0300

    Create function emnail

--- farmacia/settings.py
+++ farmacia/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'farmacia',
         'USER': 'postgres',
-        'PASSWORD': 'cursodeti27!',
+        'PASSWORD': '!',
         'HOST': 'localhost',
         'PORT': '5432'
 
